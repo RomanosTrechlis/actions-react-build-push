b440c0c7b0a8f6a78e36d2043cdc3e44c04c57e6
Romanos Trechlis <r.trechlis@gmail.com>
2022-01-27 10:43:39 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,22 +8,29 @@ prBranch='master'
 prTitle='Pull request'
 prBody='Pull request'
 
-if [ -z "${INPUT_PACKAGE_MANAGER+x}" ]; then
+echo "${packageManager}"
+echo "${buildCommand}"
+echo "${pushBranchPrefix}"
+echo "${prBranch}"
+echo "${prTitle}"
+echo "${prBody}"
+
+if [ -z ${INPUT_PACKAGE_MANAGER+x} ]; then
   packageManager="${INPUT_PACKAGE_MANAGER}";
 fi
-if [ -z "${INPUT_BUILD_COMMAND+x}" ]; then
+if [ -z ${INPUT_BUILD_COMMAND+x} ]; then
   buildCommand="${INPUT_BUILD_COMMAND}";
 fi
-if [ -z "${INPUT_PUSH_BRANCH_PREFIX+x}" ]; then
+if [ -z ${INPUT_PUSH_BRANCH_PREFIX+x} ]; then
   pushBranchPrefix="${INPUT_PACKAGE_MANAGER}";
 fi
-if [ -z "${INPUT_PR_BRANCH+x}" ]; then
+if [ -z ${INPUT_PR_BRANCH+x} ]; then
   prBranch="${INPUT_PR_BRANCH}";
 fi
-if [ -z "${INPUT_PR_TITLE+x}" ]; then
+if [ -z ${INPUT_PR_TITLE+x} ]; then
   prTitle="${INPUT_PR_TITLE}";
 fi
-if [ -z "${INPUT_PR_BODY+x}" ]; then
+if [ -z ${INPUT_PR_BODY+x} ]; then
   prBody="${INPUT_PR_BODY}";
 fi
 
