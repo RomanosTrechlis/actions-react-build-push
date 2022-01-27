8e24e4212bd93680a44910bc7604f5c4e3d15759
Romanos Trechlis <r.trechlis@gmail.com>
2022-01-27 10:40:20 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,37 @@
 #!/bin/sh
 set -e
 
-echo "${INPUT_PACKAGE_MANAGER}"
-echo "${INPUT_BUILD_COMMAND}"
-echo "${INPUT_PUSH_BRANCH_PREFIX}"
-echo "${INPUT_PR_BRANCH}"
-echo "${INPUT_PR_TITLE}"
-echo "${INPUT_PR_BODY}"
+packageManager='yarn'
+buildCommand='yarn build'
+pushBranchPrefix='action_push'
+prBranch='master'
+prTitle='Pull request'
+prBody='Pull request'
 
-yarn -v
-npm -v
-git -v
+if [ -z "${INPUT_PACKAGE_MANAGER+x}" ]; then
+  packageManager="${INPUT_PACKAGE_MANAGER}";
+fi
+if [ -z "${INPUT_BUILD_COMMAND+x}" ]; then
+  buildCommand="${INPUT_BUILD_COMMAND}";
+fi
+if [ -z "${INPUT_PUSH_BRANCH_PREFIX+x}" ]; then
+  pushBranchPrefix="${INPUT_PACKAGE_MANAGER}";
+fi
+if [ -z "${INPUT_PR_BRANCH+x}" ]; then
+  prBranch="${INPUT_PR_BRANCH}";
+fi
+if [ -z "${INPUT_PR_TITLE+x}" ]; then
+  prTitle="${INPUT_PR_TITLE}";
+fi
+if [ -z "${INPUT_PR_BODY+x}" ]; then
+  prBody="${INPUT_PR_BODY}";
+fi
+
+
+echo "${packageManager}"
+echo "${buildCommand}"
+echo "${pushBranchPrefix}"
+echo "${prBranch}"
+echo "${prTitle}"
+echo "${prBody}"
+
