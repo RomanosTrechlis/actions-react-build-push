f0912dfa9e1cd97ad6f71ba558e4fa7ce224ab2f
Romanos Trechlis <r.trechlis@gmail.com>
2022-01-27 12:36:29 +0200

    hub

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,6 @@ pushBranchPrefix=${INPUT_PUSH_BRANCH_PREFIX:-action_push}
 prBranch=${INPUT_PR_BRANCH:-master}
 prTitle=${INPUT_PR_TITLE:-Pull request}
 
-#echo "${packageManager}"
-#echo "${buildCommand}"
-#echo "${pushBranchPrefix}"
-#echo "${prBranch}"
-#echo "${prTitle}"
-#echo "${prBody}"
-
-hub version
-
-exit
-
 branch=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
 pushBranchName=${pushBranchPrefix}_${branch}
 
@@ -30,9 +19,14 @@ $buildCommand
 ls -lh
 
 git add build
+git status
 git commit -m "Github action: build"
 git push origin "$pushBranchName"
 
 prBody=git request-pull -p "$currentBranch" origin "$pushBranchName"
 body=${INPUT_PR_BODY:-"$prBody"}
 
+echo "$body"
+
+export GITHUB_USER="${GITHUB_ACTOR}"
+hub pull-request -b "$pushBranchName" -h "$prBranch" -m "$body" --no-edit
